374ad7a5e3887ddfa52e14bbd1c86e527ee2db0e
Ricky <ricky@bingzer.com>
2021-01-06 10:08:35 -0500

    more unit tests

--- tests/attv.prototypes.test.ts
+++ tests/attv.prototypes.test.ts
@@ -31,10 +31,78 @@ describe('HtmlElement.prototypes', () => {
         expect(HTMLElement.prototype.attvAttr).toBeTruthy();
     });
 
-    it('Should show the element', () => {
+    it('Should show the element (block)', () => {
         let element: HTMLElement = document.createElement('div');
         element.attvShow();
 
         expect(element.style.display).toBe('block');
+    });
+
+    it('Should show the element (inline-block)', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.style.display = 'inline-block';
+        element.attvHide();  // hide first
+
+        element.attvShow();  // then show
+
+        expect(element.style.display).toBe('inline-block');
+    });
+
+    it('Should hide the element', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.attvHide();
+
+        expect(element.style.display).toBe('none');
+    });
+
+    it('Should get attribute value (built-in attribute)', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.style.border = '1px solid black';
+
+        let received = element.attvAttr('style');
+        expect(received).toBe('border: 1px solid black;');
+    });
+
+    it('Should get attribute value (data attribute - boolean)', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.setAttribute('data-boolean', 'true');
+
+        let received = element.attvAttr('data-boolean');
+        expect(received).toBe(true);
+    });
+
+    it('Should get attribute value (data attribute - number)', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.setAttribute('data-number', '100');
+
+        let received = element.attvAttr('data-number');
+        expect(received).toBe(100);
+    });
+
+    it('Should get attribute value (data attribute - json)', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.setAttribute('data-json', '{ key: "some-value"}');
+
+        let received = element.attvAttr('data-json');
+        expect(received.key).toBe("some-value");
+    });
+
+    it('Should get all attributes', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.setAttribute('data-json', '{ key: "some-value"}');
+        element.style.border = '1px solid black';
+
+        let received = element.attvAttr();
+        console.log(received);
+        expect(received['data-json']).toBeTruthy();
+        expect(received.style).toBeTruthy();
+    });
+
+    it('Should set attribute value', () => {
+        let element: HTMLElement = document.createElement('div');
+        element.attvAttr('data-json', '{ key: "some-value"}');
+        
+        let received = element.attvAttr('data-json');
+        expect(received).toBeTruthy();
     });
 });
