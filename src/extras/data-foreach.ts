3bf5f27c25f293c63265b1cb03c9417c92e5aa61
Ricky <ricky@bingzer.com>
2021-01-30 09:03:12 -0500

    [data-foreach]

--- src/extras/data-foreach.ts
+++ src/extras/data-foreach.ts
@@ -11,18 +11,27 @@ namespace Attv.DataForEach {
         
         load(element: HTMLElement): BooleanOrVoid {
             let html = element.attvHtml();
+            let expression = this.parseExpression(element);
 
             Attv.addAttribute(DataContent.Key, element, html);
 
             // remove the content
             element.attvHtml('');
 
-            return Attv.DataForEach.render();
+            return true;
         }
-    }
 
-    export function render(): BooleanOrVoid {
-        return true;
+        private parseExpression(element: HTMLElement): { name: string, array: [] } {
+            let expression = this.attribute.raw(element);
+            let split = expression.split('in');
+            let varName = split[0].trim();
+            let varsName = split[1].trim();
+
+            return {
+                name: varName,
+                array: Attv.DataModel.getProperty(varsName)
+            };
+        }
     }
 }
 
