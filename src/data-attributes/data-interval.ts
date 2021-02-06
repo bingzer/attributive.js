66541cab5859e41d3db902cee8dada3762d44af9
Ricky Tobing <bingzer@users.noreply.github.com>
2021-02-06 14:54:30 -0500

    tests for data-interval

--- src/data-attributes/data-interval.ts
+++ src/data-attributes/data-interval.ts
@@ -9,50 +9,67 @@ namespace Attv {
             this.isAutoLoad = false;
         }
 
-        interval(element: HTMLElement, fn: () => void) {
+        interval(element: HTMLElement, fn: () => void): IntervalTimer {
             let ms = parseInt(this.raw(element));
             
-            DataInterval.run(fn, ms);
+            return DataInterval.run(fn, ms);
         }
 
-        static run(fn: () => void, ms: number) {
-            if (ms) {
-                let timer = new IntervalTimer(ms, fn);
-                DataInterval.step(timer, ms);
-            }
-
-            fn();
+        /**
+         * Runs a function with interval
+         * @param fn function to run
+         * @param ms how long? in milliseconds
+         * @param until how long until the inteval stops (in milliseconds)
+         */
+        static run(fn: () => void, ms: number, until?: number): IntervalTimer {
+            let timer = { interval: ms, until: until, fn: fn };
+            
+            return DataInterval.step(timer, ms);
         }
 
-        private static step(intervalTimer: IntervalTimer, timestamp: number) {
-            if (intervalTimer.start === undefined) {
-                intervalTimer.start = timestamp - intervalTimer.timer;
+        static stop(timer: IntervalTimer) {
+            window.cancelAnimationFrame(timer.id);
+        }
+
+        private static step(timer: IntervalTimer, timestamp: number): IntervalTimer {
+            // if no interval is defined
+            if (!timer.interval) {
+                timer.fn();
+                return timer;
             }
 
-            const elapsed = timestamp - intervalTimer.start;
-            if (elapsed > intervalTimer.timer) {
-                intervalTimer.fn();
-                intervalTimer.start = timestamp;
-                intervalTimer.id = DataInterval.requestAnimationFrame(intervalTimer);
+            if (timer.step === undefined) {
+                timer.since = Date.now();
+                timer.step = timestamp - timer.interval;
+                timer.fn();
+            }
+
+            const elapsed = timestamp - timer.step;
+            if (elapsed > timer.interval) {
+                timer.fn();
+                timer.step = timestamp;
+                timer.id = window.requestAnimationFrame(timestamp => this.step(timer, timestamp));
+
+                // -- check until
+                if (timer.until && (Date.now() - timer.since) >= timer.until) {
+                    DataInterval.stop(timer);
+                }
             }
             else {
-                intervalTimer.id = DataInterval.requestAnimationFrame(intervalTimer);
+                timer.id = window.requestAnimationFrame(timestamp => this.step(timer, timestamp));
             }
-        }
 
-        private static requestAnimationFrame(intervalTimer: IntervalTimer): number {
-            return window.requestAnimationFrame(timestamp => {
-                this.step(intervalTimer, timestamp);
-            });
+            return timer;
         }
     }
 
-    class IntervalTimer {
-        start?: number;
+    export interface IntervalTimer {
         id?: number;
-
-        constructor (public timer: number, public fn: () => void) {
-        }
+        interval?: number;
+        step?: number;
+        since?: number;
+        until?: number;
+        fn: () => void;
     }
 }
 
